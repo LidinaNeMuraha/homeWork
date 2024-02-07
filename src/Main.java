bdb005221bbad6b7308c6e82609e2b90aa90b587
Александр <kmuken@bk.ru>
2024-02-07 19:45:03 +0500

    Zdelal DZ

--- src/Main.java
+++ src/Main.java
@@ -42,6 +42,48 @@ public class Main {
 
         // zadanie 5
 
+        var frog = 3.5;
+        System.out.println(frog);
+        var frogg = frog * 10;
+        System.out.println(frogg);
+        var froggg = frogg / 3.5;
+        System.out.println(froggg);
+        var frogggg = froggg + 4;
+        System.out.println(frogggg);
+
+        // zadanie 6
+
+        var bokserOdin = 78.2;
+        var bokserDva = 82.7;
+
+        var massaObsh = bokserOdin + bokserDva;
+        System.out.println(massaObsh);
+
+        var raznica = bokserDva - bokserOdin;
+        System.out.println("1 боксер легче второго на " + raznica + " кг!");
+
+        // zadanie 7
+
+        var bolshe =12;
+        var menshe = 3;
+
+        var obsh = bolshe + menshe + 10;
+        var delenie = (bolshe + menshe) % obsh;
+        System.out.println(delenie);
+
+        // zadanie 8
+
+        var time = 640;
+        var workTime = 8;
+
+        var workers = time / workTime;
+        System.out.println("Всего работников в компании " + workers + " человек");
+
+        var workersDop = workers + 94;
+        var timePosle = workersDop * workTime;
+        System.out.println("Если в компании работает " + workersDop+ " человек, то всего " + timePosle + " часов работы может быть поделено между сотрдникками");
+
+
 
 
 
