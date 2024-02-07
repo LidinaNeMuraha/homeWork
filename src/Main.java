2a6511cafa239ff9dc242a6e5f3982db918a2794
Александр <kmuken@bk.ru>
2024-02-07 19:20:34 +0500

    sdelal 1 zadanie 2 zadanie 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        // zadanie 1
+
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
@@ -9,8 +11,41 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
 
-        var so = 2;
-        System.out.println(so);
+        // zadanie 2
+
+        var dogPlus = dog + 4;
+        var catPlus = cat + 4;
+        var paperPlus = paper + 4;
+
+        System.out.println(dogPlus);
+        System.out.println(catPlus);
+        System.out.println(paperPlus);
+
+        // zadanie 3
+
+        var DogMinus = dog - 3.5;
+        var CatMinus = cat - 1.6;
+        var PaperMinus = paper - 7639;
+
+        System.out.println(DogMinus);
+        System.out.println(CatMinus);
+        System.out.println(PaperMinus);
+
+        // zadanie 4
+
+        var friend = 19;
+        System.out.println(friend);
+        var friends = friend + 2;
+        System.out.println(friends);
+        var friendss = friends / 7;
+        System.out.println(friendss);
+
+        // zadanie 5
+
+
+
+
+
 
 
     }
